88aff816241218300f0132e1ec21ff1664edcc26
Joel <simkalok0914@gmail.com>
2020-09-21 22:42:11 +0800

    Initial commit

---
+++ Assets/Bezier.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bezier : MonoBehaviour
+{
+    public static Vector2 Curve(List<Vector2> p, double t)
+    {
+        Vector2 answer = new Vector2(0, 0);
+        for (int i = 0; i < p.Count; i++)
+        {
+            double x = p[i].x * BernsteinPolynomial(i, p.Count - 1, t);
+            double y = p[i].y * BernsteinPolynomial(i, p.Count - 1, t);
+            answer += new Vector2((float)x, (float)y);
+        }
+        return answer;
+    }
+
+    public static Vector3 Curve(List<Vector3> p, double t)
+    {
+        Vector3 answer = new Vector3(0, 0, 0);
+        for (int i = 0; i < p.Count; i++)
+        {
+            double x = p[i].x * BernsteinPolynomial(i, p.Count - 1, t);
+            double y = p[i].y * BernsteinPolynomial(i, p.Count - 1, t);
+            double z = p[i].z * BernsteinPolynomial(i, p.Count - 1, t);
+            answer += new Vector3((float)x, (float)y, (float)z);
+        }
+        return answer;
+    }
+
+    static double Exclamation(double value)
+    {
+        double answer = 1;
+        while(value > 0)
+        {
+            answer *= value--;
+        }
+        return answer;
+    }
+
+    static double Combination(double n, double i)
+    {
+        return Exclamation(n) / (Exclamation(i) * Exclamation(n - i));
+    }
+
+    static double BernsteinPolynomial(double i, double n, double t)
+    {
+        return Combination(n, i) * Math.Pow(t,i) * Math.Pow(1 - t, n - i);
+    }
+}
---
+++ Assets/GenerateCurve.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GenerateCurve : MonoBehaviour
+{
+    public LineRenderer line;
+    public GameObject dot;
+
+    Vector3[] points;
+    List<Vector3> listPoints = new List<Vector3>();
+
+    [Range(0,1)]
+    public float time;
+
+    LineRenderer selfLine;
+    List<Vector3> curveLine = new List<Vector3>();
+    Vector3[] curvePoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // define the points array size
+        points = new Vector3[line.positionCount];
+
+        // get all vertices from line renderer and put it inside array
+        line.GetPositions(points);
+
+        // convert array into list
+        foreach (Vector3 item in points)
+        {
+            listPoints.Add(item);
+        }
+
+        // draw curve line
+        for (float i = 0; i <= 1; i = i + 0.01f)
+        {
+            Vector3 p = Bezier.Curve(listPoints, i);
+            curveLine.Add(p);
+        }
+        curvePoints = new Vector3[curveLine.Count];
+        for (int i = 0; i < curveLine.Count; i++)
+        {
+            curvePoints[i] = curveLine[i];
+        }
+        selfLine = GetComponent<LineRenderer>();
+        selfLine.positionCount = curveLine.Count;
+        selfLine.SetPositions(curvePoints);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dot.transform.position = Bezier.Curve(listPoints, time);
+    }
+}
